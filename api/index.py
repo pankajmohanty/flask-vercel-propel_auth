6fdadda2123c379ed5f8730b215c6c7b0a52fce0
pankajmohanty <51083695+pankajmohanty@users.noreply.github.com>
2024-03-26 11:26:42 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify
+from propelauth_flask import init_auth
+
+auth = init_auth(
+    "YOUR_AUTH_URL",
+    "YOUR_API_KEY",
+)            
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "Flask Vercel Example - Hello World", 200
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return jsonify({"status": 404, "message": "Not Found"}), 404
+@app.route("/api/whoami")
+@auth.require_user
+def who_am_i():
+    """This route is protected, current_user is always set"""
+    return {"user_id": current_user.user_id}
